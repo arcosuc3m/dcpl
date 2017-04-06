78941ede8bc7615ada38221b4de89d4c939afb3f
FranciscoRguezM <100317282@alumnos.uc3m.es>
2017-04-06 15:42:19 +0200

    Operador de acceso aleatorio implementado

--- test/dcpl.cpp
+++ test/dcpl.cpp
@@ -26,12 +26,13 @@ namespace dcpl{
 		int type = 0;		//splitting type (block, round robin...)
 		int rr_param = 1;	//if the type is rr, chunk's size (ignored otherwise)
 
-		int size = 0;		//size of the dataType which symbolizes the splitting in file (in elements of tipe double or int)
+		int datatype_size = 0;		//size of the dataType which symbolizes the splitting in file (in elements of tipe double or int)
+		int vector_size = 0;
 	} schedule_data; //datos del reparto
 
 	int noop = MPI_Init(nullptr, nullptr); //TODO: preguntar como hacer esto con argc y argv
 	/*********************/
-	//vector
+	/********VECTOR********/
 	template <typename T>
 	class DistributedVector{
 		private:
@@ -39,8 +40,8 @@ namespace dcpl{
 			vector<T> contenido{}; 	//DATA
 			schedule_data my_schedule{BLOCK, 0};//spliting schedule
 			MPI_Datatype datatype;
-			/*********************/
-
+			T dummy; //fields use in order to be able to return a reference in operator[]
+			/********************/
 			static inline MPI_Datatype CHECK_TYPE(){
 				return (std::is_same<int, T>::value )?(MPI_INT):MPI_DOUBLE;
 			}
@@ -51,31 +52,29 @@ namespace dcpl{
 				MPI_Datatype res;
 				int num_proc{MPI_Comm_size_wrapper()};
 				int rango{MPI_Comm_rank_wrapper()};
-				int elements = size/sizeof(T);
-				int amount_per_process = elements/num_proc;
+				my_schedule.vector_size = size/sizeof(T);
+				int amount_per_process = my_schedule.vector_size/num_proc;
 				int blocklength, dis[1], last_length;
 				int contador = 0;
 				vector<int> blocklengths{};
-				vector<MPI_Aint> displacements{};
-
-			
+				vector<MPI_Aint> displacements{};			
 				switch(my_schedule.type){
 					case BLOCK:					
 						//displacement = rank*elems for process 0
 						dis[0] = rango*amount_per_process;						
-						last_length = amount_per_process+elements%num_proc;
+						last_length = amount_per_process+my_schedule.vector_size%num_proc;
 						blocklength = (rango != num_proc-1)?amount_per_process:last_length;
 						MPI_Type_create_indexed_block(1, blocklength, dis, CHECK_TYPE(), &res);
 						MPI_Type_commit(&res);
-						this->my_schedule.size = blocklength;
+						this->my_schedule.datatype_size = blocklength;
 						break;
 					case ROBIN:
 						auto init = chrono::system_clock::now();
 						contador = rango*my_schedule.rr_param;
 						//prueba rendimiento 200.000.000 rodajas = 3683 ms por proceso. (se supone paralelo)
-						while(contador < elements){
-							int length = (elements - contador < my_schedule.rr_param) ? (elements - contador) :( my_schedule.rr_param);
-							my_schedule.size += length;
+						while(contador < my_schedule.vector_size){
+							int length = (my_schedule.vector_size - contador < my_schedule.rr_param) ? (my_schedule.vector_size - contador) :( my_schedule.rr_param);
+							my_schedule.datatype_size += length;
 							blocklengths.push_back(length);
 							displacements.push_back(contador*sizeof(T));
 							contador += (my_schedule.rr_param*num_proc);							
@@ -90,6 +89,35 @@ namespace dcpl{
 				}
 				this->datatype = res;
 			}
+			/*
+				@param pos -> position in the vector which owner process wanna be known
+				@return the rank of the process which has that element in memory
+			*/
+			int owner(int pos){
+				int num_proc = MPI_Comm_size_wrapper();
+				switch(my_schedule.type){
+					case BLOCK:
+					return (pos/(my_schedule.vector_size/num_proc) >= num_proc)? num_proc-1 : pos/(my_schedule.vector_size/num_proc);
+					break;
+					case ROBIN:
+					return (pos/my_schedule.rr_param)%num_proc;
+					break;
+				}
+				return 0;
+			}
+			int global_to_local_pos(int pos){
+				int rango = MPI_Comm_rank_wrapper();
+				int num_proc = MPI_Comm_size_wrapper();
+				switch(my_schedule.type){					
+					case BLOCK:
+						return (pos - rango*my_schedule.vector_size);
+					break;
+					case ROBIN:
+						return (pos/(my_schedule.rr_param*num_proc))*my_schedule.rr_param  + (pos - ((pos/my_schedule.rr_param)*my_schedule.rr_param));
+					break;					
+				}
+				return 0;
+			}
 		public:
 			DistributedVector(schedule_type tipo){
 				my_schedule.type = tipo;				
@@ -98,20 +126,32 @@ namespace dcpl{
 				 this->my_schedule.type = tipo;
 				 this->my_schedule.rr_param = rr_param;
 			};
+			T& operator[](int pos){						
+				int local_pos = global_to_local_pos(pos);
+				if(owner(pos) == MPI_Comm_rank_wrapper()){ //soy el que almacena el proceso
+					MPI_Bcast(&contenido[local_pos], 1, CHECK_TYPE(), MPI_Comm_rank_wrapper(), MPI_COMM_WORLD);					
+					return contenido[local_pos];
+				}else{
+					MPI_Bcast(&(this->dummy), 1, CHECK_TYPE(), owner(pos), MPI_COMM_WORLD);
+					return this->dummy;
+				}
+			}
+			int size(){
+				return my_schedule.vector_size;
+			}
 			/*
 				@param path -> path to the file to read
-
 			*/
 			void llenar(const char* path){
 				MPI_File file_descriptor;
-				MPI_Offset file_size;					
+				MPI_Offset file_size;
 				MPI_Status status;
 				MPI_File_open(MPI_COMM_WORLD, path, MPI_MODE_RDONLY, MPI_INFO_NULL, &file_descriptor);
 				MPI_File_get_size(file_descriptor, &file_size);
 				build_datatype(file_size);
-				contenido.resize(my_schedule.size);
+				contenido.resize(my_schedule.datatype_size);
 				MPI_File_set_view(file_descriptor, 0, MPI_CHAR, datatype, "native", MPI_INFO_NULL);
-				MPI_File_read(file_descriptor, contenido.data(), my_schedule.size, CHECK_TYPE(), &status);
+				MPI_File_read(file_descriptor, contenido.data(), my_schedule.datatype_size, CHECK_TYPE(), &status);
 				usleep(10000*MPI_Comm_rank_wrapper());
 				cout << "=========" << MPI_Comm_rank_wrapper()<< "=========" << endl;
 				for(auto ii : contenido){
@@ -120,16 +160,21 @@ namespace dcpl{
 				MPI_File_close(&file_descriptor);
 				MPI_Barrier(MPI_COMM_WORLD);
 			}
+			/*
+				@param path -> path to the file to write the vector
+			*/
 			void write(const char* path){
 				MPI_File file_descriptor;
 				//MPI_Status status;
 				MPI_File_open(MPI_COMM_WORLD, path, MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &file_descriptor);																
 				MPI_File_set_view(file_descriptor, 0, MPI_CHAR, datatype, "native", MPI_INFO_NULL);
-				MPI_File_write_all_begin(file_descriptor, contenido.data(), my_schedule.size, CHECK_TYPE());				
+				MPI_File_write_all_begin(file_descriptor, contenido.data(), my_schedule.datatype_size, CHECK_TYPE());				
 			}
 			~DistributedVector(){
 				
 			};	
+	/*********************/
+
 	};
 
 }
--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,15 @@ using namespace dcpl;
 int main(){
 	DistributedVector<double> v{ROBIN, 1};
 	v.llenar("DATA");
-	v.write("DATA2");
+
+	for(int ii = 0; ii < v.size()/2; ++ii){
+		auto aux = v[ii];
+		v[ii] = v[v.size()-(ii+1)];
+		v[v.size()-(ii+1)] = aux;
+	}
+
+	for(int ii = 0; ii < v.size(); ++ii){
+		cout << "v[" << ii << "]" << v[ii] << endl;
+	}
 	return 0;
 }
